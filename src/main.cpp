6b1556de283c9a0d00e766b787d0bb0de1f9a2ae
w3-griffin <william3.griffin@live.uwe.ac.uk>
2025-06-09 19:01:43 +0100

    Remove support for SD card and now usig SPIFFS to use the built in flash storage on the esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,62 @@
 #include <WiFi.h>
 #include <WebServer.h>
-#include <SdFat.h>
-#include <SPI.h>
+#include <SPIFFS.h>
 #include "obd.hpp"
 
-// Embedded HTML
-#include "html_embedded/index_html.h"
-#include "html_embedded/dtc_html.h"
-#include "html_embedded/nodtc_html.h"
-
-#define SD_CS_PIN A0
-SdFat SD;
-WebServer server(80);
-
-// WiFi settings
 const char* ssid = "OBD_Reader";
 const char* password = "12345678";
 
-// Save file to SD if not exists
-void saveIfMissing(const char* path, const char* content) {
-    if (!SD.exists(path)) {
-        FsFile file = SD.open(path, O_WRITE | O_CREAT);
-        if (file) {
-            file.print(content);
-            file.close();
-            Serial.printf("Saved %s to SD\n", path);
-        } else {
-            Serial.printf("Failed to write %s\n", path);
-        }
-    }
-}
+WebServer server(80);
 
-// Read HTML from SD card
-String loadFromSD(const char* path) {
-    FsFile file = SD.open(path);
-    if (!file) return "File not found.";
-    String html;
-    while (file.available()) html += (char)file.read();
-    file.close();
-    return html;
+// Load file from SPIFFS
+String loadFile(const char* path) {
+  File file = SPIFFS.open(path, "r");
+  if (!file || file.isDirectory()) {
+    return "File not found.";
+  }
+  String content = file.readString();
+  file.close();
+  return content;
 }
 
 void handleRoot() {
-    server.send(200, "text/html", loadFromSD("/index.html"));
+  server.send(200, "text/html", loadFile("/index.html"));
 }
 
 void handleDTC() {
-    String dtcs;
-    if (obd.readTroubleCodes(dtcs)) {
-        String page = loadFromSD("/dtc.html");
-        page.replace("{{DTC}}", dtcs);
-        server.send(200, "text/html", page);
-    } else {
-        server.send(200, "text/html", loadFromSD("/nodtc.html"));
-    }
+  String dtcs;
+  if (obd.readTroubleCodes(dtcs)) {
+    String page = loadFile("/dtc.html");
+    page.replace("{{DTC}}", dtcs);
+    server.send(200, "text/html", page);
+  } else {
+    server.send(200, "text/html", loadFile("/nodtc.html"));
+  }
 }
 
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
+  Serial.begin(115200);
+  delay(1000);
 
-    if (!SD.begin(SD_CS_PIN, 1000000)) {
-        Serial.println("SD init failed!");
-        return;
-    }
-    Serial.println("SD OK");
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS mount failed!");
+    return;
+  }
+  Serial.println("SPIFFS mounted.");
 
-    // Save embedded HTML if requireddd
-    saveIfMissing("/index.html", index_html);
-    saveIfMissing("/dtc.html", dtc_html);
-    saveIfMissing("/nodtc.html", nodtc_html);
+  if (!obd.initialise()) {
+    Serial.println("OBD init failed.");
+  }
 
-    if (!obd.initialise()) {
-        Serial.println("OBD init failed");
-    }
+  WiFi.softAP(ssid, password);
+  Serial.println("AP IP: " + WiFi.softAPIP().toString());
 
-    WiFi.softAP(ssid, password);
-    Serial.println("WiFi AP: " + WiFi.softAPIP().toString());
-
-    server.on("/", handleRoot);
-    server.on("/dtc", handleDTC);
-    server.begin();
+  server.on("/", handleRoot);
+  server.on("/dtc", handleDTC);
+  server.begin();
+  Serial.println("Server started.");
 }
 
 void loop() {
-    server.handleClient();
+  server.handleClient();
 }
